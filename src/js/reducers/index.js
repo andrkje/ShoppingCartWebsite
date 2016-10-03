0180922cebe8a394e31a5844337a13cf3adc76f3
Andreas Kjerstad <andreas.pi@hotmail.com>
2016-10-03 21:43:12 +0200

    Add TodoListsReducer to reducers/index

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import todo from './TodoReducer';
 import todos from './TodosReducer';
+import todoLists from './TodoListsReducer';
 
 export default combineReducers({
     todo,
     todos,
+    todoLists
 });
